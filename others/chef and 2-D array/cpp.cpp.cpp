9fa8840ded3b38cab812cf26650961897a1d09f7
himalay laxman bhandari <46533179+himalay560@users.noreply.github.com>
2019-03-04 22:56:34 +0530

    Add files via upload

---
+++ others/chef and 2-D array/cpp.cpp.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n][n];
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin>>a[i][j];
+    long long int b[n][n];
+    b[0][0]=a[0][0];
+    for(int i=1;i<n;i++)
+        b[i][0]=b[i-1][0]+a[i][0];
+    for(int j=0;j<n;j++)
+        b[0][j]=b[0][j-1]+a[0][j];
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<n;j++)
+        {
+            b[i][j]=a[i][j] + min(b[i-1][j],b[i][j-1]);
+        }
+    }
+    cout<<b[n-1][n-1];
+}
