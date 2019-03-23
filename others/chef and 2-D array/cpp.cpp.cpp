546bd5d280e5afc4200a340e2f8bc21f3fa294b4
himalay laxman bhandari <46533179+himalay560@users.noreply.github.com>
2019-03-23 21:28:07 +0530

    Update cpp.cpp.cpp

--- others/chef and 2-D array/cpp.cpp.cpp
+++ others/chef and 2-D array/cpp.cpp.cpp
@@ -12,7 +12,7 @@ int main()
     b[0][0]=a[0][0];
     for(int i=1;i<n;i++)
         b[i][0]=b[i-1][0]+a[i][0];
-    for(int j=0;j<n;j++)
+    for(int j=1;j<n;j++)
         b[0][j]=b[0][j-1]+a[0][j];
     for(int i=1;i<n;i++)
     {
